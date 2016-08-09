c2a35d05410358c3e42afb16c598401e3a3a01c3
hosy <hosy@nurigo.net>
2016-08-09 15:24:56 +0900

    setup.py 추가

--- setup.py
+++ setup.py
@@ -6,23 +6,20 @@ except ImportError:
 setup(
     name='coolsms_python_sdk',
     version='2.0', 
-    packages=['sdk', 'api'], 
+    packages=['sdk', 'sdk/api'], 
 	package_dir={'sdk': 'sdk', 'api': 'sdk/api'},
-    #package_data={'examples': ['examples/*']},
     license='MIT License',
-    author='nurigo',
+    author='Nurigo',
     author_email='sms-team@nurigo.net',
     url='https://github.com/coolsms/python-sdk',
-    description='Send Message, Kakao Alimtalk, Message Management using PHP and REST API.',
+	download_url = 'https://github.com/coolsms/python-sdk/releases',
+    description='Send Message, Kakao Alimtalk and Management Message using Python and REST API.',
     keywords=['sms', 'rest', 'restapi', 'restsms', 'smsrest', 'coolsms', 'nurigo', 'python sdk', 'alimtalk', 'kakao'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
 		'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
-        'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
